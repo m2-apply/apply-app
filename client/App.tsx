710e3b834ae66ae0d36460bb424fbfdb5cb0cbbe
ryliep <rylie.pereira@gmail.com>
2023-08-29 13:45:05 -0700

    websocket connection established

--- client/App.tsx
+++ client/App.tsx
@@ -1,8 +1,26 @@
 import React, { useState, useReducer } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SockJS from 'sockjs-client';
 
 const App = () => {
-  return <div>Hello World</div>;
+  var sock = new SockJS('https://www.seismicportal.eu/standing_order');
+
+  sock.onopen = function () {
+    console.log('connected');
+  };
+
+  let message;
+  sock.onmessage = function (e) {
+    let msg = JSON.parse(e.data);
+    message = msg;
+
+    console.log('message received : ', msg);
+  };
+
+  sock.onclose = function () {
+    console.log('disconnected');
+  };
+  return <div>hello</div>;
 };
 
 export default App;
