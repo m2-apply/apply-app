affd299e70e6e7cc48c5fba3e21faf1a73e931df
Haoyu <liu_haoyu@berkeley.edu>
2023-08-30 09:57:26 -0700

    setup google oauth

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 import '../styles/LandingPage';
 import logo from '../assets/Apply.png';
 import { GlobalContext } from '../../state';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
+const CLIENT_ID = "697388643116-ej84pi3l2ahgf8mlu946k9uu6achff46.apps.googleusercontent.com";
 
 const LandingPage = () => {
   const { setLoggedIn } = useContext(GlobalContext);
@@ -20,6 +23,35 @@ const LandingPage = () => {
         <button className='genericButton blue' onClick={handleLogin}>
           Login
         </button>
+        <GoogleOAuthProvider clientId={CLIENT_ID}>
+          <GoogleLogin
+            onSuccess={credentialResponse => {
+              console.log(credentialResponse);
+              const decoded = jwt_decode(credentialResponse.credential || '');
+              console.log(decoded);
+              setLoggedIn(true);
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
+        </GoogleOAuthProvider>
+        {/* <div id="g_id_onload"
+          data-client_id="697388643116-ej84pi3l2ahgf8mlu946k9uu6achff46.apps.googleusercontent.com"
+          data-context="signin"
+          data-ux_mode="popup"
+          data-login_uri="http://localhost:8080/map"
+          data-auto_prompt="false">
+        </div>
+
+        <div class="g_id_signin"
+          data-type="standard"
+          data-shape="pill"
+          data-theme="outline"
+          data-text="signin_with"
+          data-size="large"
+          data-logo_alignment="left">
+        </div> */}
         <button className='genericButton white'>Sign Up</button>
       </div>
     </div>
