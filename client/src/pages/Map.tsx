20d1be04314b2918a31f77fc77193c250f94202b
Bgregz <bgregoire@live.com>
2023-08-30 16:11:03 -0400

    Print

--- client/src/pages/Map.tsx
+++ client/src/pages/Map.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
 import '../styles/Map';
 import USAMap from '../assets/NorthAmerica2.png';
 import { GlobalContext } from '../../state';
@@ -16,8 +17,9 @@ const MapView = () => {
     curPointer,
     settingNewPoint,
     setSettingNewPoint,
+    
   } = useContext(GlobalContext);
-
+const [pointer, setpointer]= useState([]);
   const handleNewLocation = () => {
     !settingNewPoint ? setSettingNewPoint(true) : setSettingNewPoint(false);
   };
@@ -35,13 +37,42 @@ const MapView = () => {
     Board.push(<Row y={i} />);
   }
 
+  const canvasWidth = 1350
+  const canvasHeight = 410
+  
+
+  // Calculate the width and height of the canvas based on the aspect ratio and desired canvas width
+  
+
+  useEffect(() => {
+    const canvas = document.getElementById('ellipseCanvas') as HTMLCanvasElement;
+    const ctx = canvas?.getContext('2d');
+
+    const lat = 28.674861072043658;
+    const lon = -81.7639904085645;
+    const x = ((lon + 180) * (1350 / 360) * 2);
+    const y = 410 - ((lat * (410 / 90)) * 2);
+
+    if (ctx) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      ctx.fillStyle = 'rgba(255, 0, 255, 0.8)';
+      ctx.beginPath();
+      ctx.ellipse(x, y, 10, 10, 0, 0, Math.PI * 2);
+      ctx.fill();
+      ctx.closePath();
+    }
+  }, []);
+
   return (
     <div className='map'>
       <div
         className={`mapCanvas ${settingNewPoint ? 'setting' : ''}`}
         style={{ position: 'relative' }}>
         <img src={USAMap} width='1350' />
-        <div className='board'>{Board}</div>
+        <div className='board'>{Board}
+          {/* <canvas id='ellipseCanvas' width={canvasWidth} height={canvasHeight}> </canvas> */}
+        </div>
         <OurPoint x={curPointer[0]} y={curPointer[1]} />
       </div>
 
