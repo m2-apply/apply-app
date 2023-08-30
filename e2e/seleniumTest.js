ab6cd41ef3f03d06ecfd3f80f557df756be2d779
Haoyu <liu_haoyu@berkeley.edu>
2023-08-30 15:44:35 -0700

    simple tests pass

--- e2e/seleniumTest.js
+++ e2e/seleniumTest.js
@@ -1,51 +1,38 @@
 const webdriver = require('selenium-webdriver');
-const { By, until } = webdriver;
+const { By, until, Builder } = webdriver;
+const assert = require("assert");
 
 async function HomePageTest(driver) {
-    let loadingPage = await driver.findElement(By.className('landingPage'));
-    let genericButton_blue = await driver.findElement(By.className('genericButton blue'));
-    let genericButton_white = await driver.findElement(By.className('genericButton white'));
+    let title = await driver.getTitle();
+    assert.equal("Document", title);
+    let loadingPage = await driver.findElement(By.className('landingPage')).isDisplayed();
+    assert.equal(loadingPage, true);
+    let login_button = await driver.findElement(By.className('genericButton blue')).isDisplayed();
+    assert.equal(login_button, true);
+    let signup_button = await driver.findElement(By.className('genericButton white')).isDisplayed();
+    assert.equal(signup_button, true);
     console.log('loadingPage sucess!');
 }
 
-async function localHostedTest(driver, servType, dbType, dbUri, dbName, dbDesc) {
-    await driver.findElement(By.id('serv-type')).sendKeys(servType);
-    await driver.findElement(By.id('db-type')).sendKeys(dbType);
-    await driver.findElement(By.id('db-uri')).sendKeys(dbUri);
-    await driver.findElement(By.id('db-name')).sendKeys(dbName);
-    await driver.findElement(By.id('db-desc')).sendKeys(dbDesc);
-    await driver.findElement(By.xpath('//button[text()="Submit"]')).click();
+
+async function loginTest(driver, username, email, password, passwordConfirm) {
+    let login_button = await driver.findElement(By.className('genericButton blue'));
+    await login_button.click();
+    console.log('login sucess!');
 }
 
-async function cloudHostedTest(
-    driver,
-    servType,
-    awsInstance,
-    awsRegion,
-    awsAccessKey,
-    awsSecretAccessKey,
-    awsURL,
-    awsName,
-    dbDesc
-) {
-    await driver.findElement(By.id('serv-type')).sendKeys(servType);
-    await driver.findElement(By.id('aws-instance')).sendKeys(awsInstance);
-    await driver.findElement(By.id('aws-region')).sendKeys(awsRegion);
-    await driver.findElement(By.id('aws-access-key')).sendKeys(awsAccessKey);
-    await driver.findElement(By.id('aws-secret-access-key')).sendKeys(awsSecretAccessKey);
-    await driver.findElement(By.id('aws-url')).sendKeys(awsURL);
-    await driver.findElement(By.id('aws-name')).sendKeys(awsName);
-    await driver.findElement(By.id('db-desc')).sendKeys(dbDesc);
-    await driver.findElement(By.xpath('//button[text()="Submit"]')).click();
-}
-
-async function signinTest(driver, username, email, password, passwordConfirm) {
-    await driver.wait(until.elementLocated(By.className('main-routes')), 30000);
-    await driver.findElement(By.id('username')).sendKeys(username);
-    await driver.findElement(By.id('email')).sendKeys(email);
-    await driver.findElement(By.id('password')).sendKeys(password);
-    await driver.findElement(By.id('passwordConfirm')).sendKeys(passwordConfirm);
-    await driver.findElement(By.xpath('//button[text()="Sign Up"]')).click();
+async function mapPageTest(driver) {
+    let map = await driver.findElement(By.className('map')).isDisplayed();
+    assert.equal(map, true);
+    let set_new_point_button = await driver.findElement(By.className('genericButtonMap white'));
+    await set_new_point_button.click();
+    let mapCanvas = await driver.findElement(By.className('mapCanvas setting')).isDisplayed();
+    assert.equal(mapCanvas, true);
+    let logout_button = await driver.findElement(By.className('genericButtonMap white'));
+    await logout_button.click();
+    let loadingPage = await driver.findElement(By.className('landingPage')).isDisplayed();
+    assert.equal(loadingPage, true);
+    console.log('mapPage sucess!');
 }
 
 (async function () {
@@ -53,8 +40,11 @@ async function signinTest(driver, username, email, password, passwordConfirm) {
     await driver.get('http://localhost:8080/');
 
     try {
-        await driver.wait(until.elementLocated(By.className('landingPage')), 30000);
         await HomePageTest(driver);
+        await driver.manage().setTimeouts({ implicit: 500000 });
+        await loginTest(driver);
+        await driver.manage().setTimeouts({ implicit: 500000 });
+        await mapPageTest(driver);
         // await driver.findElement(By.className('card')).click();
         // await driver.wait(until.elementLocated(By.className('add-container')), 10000);
         // await driver.findElement(By.className('env-button2')).click();
@@ -111,6 +101,41 @@ async function signinTest(driver, username, email, password, passwordConfirm) {
         console.error('E2E test failed:', error);
     } finally {
         await driver.executeScript('window.close()');
-        await driver.quit();
+        //await driver.quit();
     }
 })();
+
+// const {By, Builder, Browser} = require('selenium-webdriver');
+// const {suite} = require('selenium-webdriver/testing');
+// const assert = require("assert");
+
+// suite(function (env) {
+//   describe('First script', function () {
+//     let driver;
+
+//     before(async function () {
+//       driver = await new Builder().forBrowser('chrome').build();
+//     });
+
+//     after(async () => await driver.quit());
+
+//     it('First Selenium script', async function () {
+//       await driver.get('http://localhost:8080/');
+
+//       let title = await driver.getTitle();
+//       assert.equal("Web form", title);
+
+//       await driver.manage().setTimeouts({implicit: 500});
+
+//       let textBox = await driver.findElement(By.name('my-text'));
+//       let submitButton = await driver.findElement(By.css('button'));
+
+//       await textBox.sendKeys('Selenium');
+//       await submitButton.click();
+
+//       let message = await driver.findElement(By.id('message'));
+//       let value = await message.getText();
+//       assert.equal("Received!", value);
+//     });
+//   });
+// }, { browsers: [Browser.CHROME, Browser.FIREFOX]});
